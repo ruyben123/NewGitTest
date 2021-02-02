9f6def79cb6940dfc47f0257842cead62da8f472
rubin <adventorousguy_2009@yahoo.com>
2021-02-03 00:49:49 +1100

    added from seondary location

--- java_msg2.java
+++ java_msg2.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is secondary location");
+        System.out.println("Edited from secondary location");
     }
 }
