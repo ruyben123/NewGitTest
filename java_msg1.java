c369cc7acbc3d21d3e6249ea6a2df295c7a3f26e
rubin <adventorousguy_2009@yahoo.com>
2021-02-04 11:53:44 +1100

    changed classname from 'main' to 'hello'

--- java_msg1.java
+++ java_msg1.java
@@ -1,4 +1,4 @@
-public class Main {
+public class hello {
     public static void main(String[] args) {
         System.out.println("This is main location");
         System.out.println("Added a msg from main location");
